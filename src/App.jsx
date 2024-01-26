c57ba8d3a8dee5be059da02f12e80dd4ee7fb33d
atikdev <atikdev.bd@gmail.com>
2024-01-26 22:46:53 +0600

    modal open and close done control with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
 import MovieList from "./components/cine/MovieList";
@@ -5,13 +6,14 @@ import MovieList from "./components/cine/MovieList";
 function App() {
   return (
     <>
-      <div>
-        <Header />
+      <Header />
+      <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <SideBar />
           <MovieList />
         </div>
-      </div>
+      </main>
+      <Footer />
     </>
   );
 }
